c2bb1d4f6604667d9352d18a490cb751bfffa0f7
ronit <ronit@precanto.com>
2022-05-03 13:57:22 -0700

    starting main

--- rag.c
+++ rag.c
@@ -1,5 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #define NLOCK = 10
 #define NPROC = 20
+int main(int argc, char *argv[])
+{
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        printf("Please provide valid file");
+        exit(0);
+    }
+}
 
 void rag_init()
 {
