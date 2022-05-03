5581e82848d7c2d7a1cb830b3ba4eb9e3528a3ee
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-05-03 16:56:31 -0400

    Started working on rag.c

--- rag.c
+++ rag.c
@@ -1,8 +1,14 @@
-#define NLOCK = 10
-#define NPROC = 20
+#define NLOCK 10
+#define NPROC 20
+#include <stdlib.h>
+int **adjacencyMatrix;
 
 void rag_init()
 {
+    adjacencyMatrix = (int **)malloc(NPROC * sizeof(int *));
+    for(int i = 0; i < NPROC; i++) {
+        adjacencyMatrix[i] = (int *)malloc(NPROC * sizeof(int));
+    }
 }
 
 void rag_request(int pid, int lockid)
@@ -15,4 +21,13 @@ void rag_alloc(int pid, int lockid)
 
 void dealloc()
 {
+}
+
+void rag_print() 
+{
+    for(int i = 0; i < NPROC; i++) {
+        for(int j = 0; j < NPROC; j++) {
+            printf("%d ", adjacencyMatrix[i][j]);
+        }
+    }
 }
